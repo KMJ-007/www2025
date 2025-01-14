1a3606f4f524c2f831ad724d6fdb5e52951834e8
karan <karanjanthe@gmail.com>
2025-01-14 19:56:34 +0530

    something is working
    Signed-off-by: karan <karanjanthe@gmail.com>

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -1,24 +1,11 @@
 'use client';
 
-import { motion } from 'framer-motion';
-
 export default function Bio() {
   return (
-    <motion.div 
-      className="space-y-4"
-      initial={{ opacity: 0, y: 20 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.5 }}
-    >
-      <p className="text-sm text-[#ededed90] leading-relaxed">
-        Serial key presser by day, automation enthusiast by night.
+    <blockquote className="border-l-4 border-gray-300 pl-4 italic">
+      <p>
+        Serial key presser by day, factorio player by night.
       </p>
-
-      {/* <p className="text-sm text-[#ededed70] leading-relaxed">
-        I love hosting <Link className="underline" href="https://x.com/KaranJanthe/status/1872936041059107161/"> meetups</Link> and <Link className="underline" href="https://wtfuckathon.vercel.app/">stupidathon</Link>.
-      </p> */}
-
-   
-    </motion.div>
+    </blockquote>
   );
-} 
+}
