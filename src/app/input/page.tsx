48ff35082851cdc19c3932a66427f6b73e49fa1a
karan <karanjanthe@gmail.com>
2025-01-14 18:37:16 +0530

    added input
    Signed-off-by: karan <karanjanthe@gmail.com>

--- src/app/input/page.tsx
+++ src/app/input/page.tsx
@@ -1,26 +1,13 @@
+import { MDXRemote } from 'next-mdx-remote/rsc';
+import { getPostBySlug } from '@/lib/mdx';
 
-/**
- * 
- * blogs which i keep revisiting very time:
- * https://www.ribbonfarm.com/2014/11/05/dont-surround-yourself-with-smarter-people/
- * 
- * 
- * few newsletter and blog rolls which i consume more:
- * 
- * books:
- * 
- * movies:
- * 
- */
+export default async function InputPage() {
+  const { content } = await getPostBySlug('input');
 
-export default function InputPage() {
   return (
     <div className="flex-1 w-full max-w-screen-xl mx-auto mt-6">
-      <h1 className="font-mono text-2xl sm:text-3xl text-neutral-200 tracking-wider mb-12">
-        Input
-      </h1>
-      <div className="space-y-8">
-        Hello
+      <div className="prose prose-invert max-w-none">
+        <MDXRemote source={content} />
       </div>
     </div>
   );
